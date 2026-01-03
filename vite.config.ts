9bb5c3e0b0905835846f0ad718ea86e5f326b140
opayalfaqeeh <opayalfaqeeh@gmail.com>
2026-01-03 15:10:11 +0200

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,12 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  // استخدام base النسبي لضمان عمل الموقع سواء كان في المجلد الرئيسي أو فرعي على GitHub
+  // استخدام المسار النسبي لضمان عمل الروابط بشكل صحيح على GitHub Pages
   base: './',
+  plugins: [react()],
   build: {
     outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false
   }
 });
